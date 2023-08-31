3292f5353d1ecb0a92f99fd04c107a40c4f1604b
Matheus Lopes <inolopesm@gmail.com>
2023-08-31 15:13:07 -0300

    feat: create force extraction route with api key guard

---
+++ src/api-key.guard.ts
@@ -0,0 +1,21 @@
+import {
+  Injectable,
+  type CanActivate,
+  type ExecutionContext,
+} from "@nestjs/common";
+
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  private readonly apiKey: string;
+
+  constructor(configService: ConfigService) {
+    this.apiKey = configService.getOrThrow("API_KEY");
+  }
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    return request.headers["x-api-key"] === this.apiKey;
+  }
+}
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
+import { ApiKeyGuard } from "./api-key.guard";
 
 @Controller()
 export class AppController {
+  @UseGuards(ApiKeyGuard)
+  @Get("force-extraction")
+  async forceExtraction() {
+    return true;
+  }
+
   @Get("health")
   async health() {
     return { message: "ok" };
